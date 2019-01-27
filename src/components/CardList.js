2256efa65ad5673032675f677a95f6decb61a35b
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-01-27 07:55:31 -0800

    Init Commit

---
+++ src/components/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  console.log('CardList');
+  return (
+    <div>
+      {robots.map((user, i) => (
+        <Card key={i} id={user.id} name={user.name} email={user.email} />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
