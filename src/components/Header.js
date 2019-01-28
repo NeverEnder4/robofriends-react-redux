93fb3c60ebee35199d264ed9f156cdf0634a8891
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-01-27 19:16:49 -0800

    add unit, integration and snapshot testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CounterButton from './CounterButton';
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -9,6 +10,7 @@ class Header extends Component {
     console.log('Header');
     return (
       <div>
+        <CounterButton color="red" />
         <h1 className="f1">RoboFriends</h1>
       </div>
     );
